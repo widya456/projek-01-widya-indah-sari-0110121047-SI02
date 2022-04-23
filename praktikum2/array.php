c8947c92f213afd6b0f3e29190f4746390a6e13b
widya456 <93046108+widya456@users.noreply.github.com>
2022-04-23 12:17:07 +0700

    Add files via upload

---
+++ praktikum2/array.php
@@ -0,0 +1,15 @@
+<?php
+    $data = array('ravi','dika','fiki'.'budi');
+    $data2 = ['rafi','ucup','nelan',10,true,30.5,];
+
+    $buah = [];
+
+
+    $buah[0] = "apel";
+    $buah[1] = "mangga";
+    $buah[2] = "jeruk";
+
+    print_r($buah);
+    echo $buah[2];
+    echo count($buah);
+?>
